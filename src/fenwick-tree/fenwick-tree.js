0141bc2a5e59d45e662adfcea695618278e1326b
Evgeny Kazakov <kazakov@arrival.com>
2022-08-06 16:49:22 +0200

    WIP SAVEPOINT

--- src/fenwick-tree/fenwick-tree.js
+++ src/fenwick-tree/fenwick-tree.js
@@ -1,3 +1,8 @@
+// https://github.com/williamfiset/Algorithms/blob/master/src/main/java/com/williamfiset/algorithms/datastructures/fenwicktree/FenwickTreeRangeQueryPointUpdate.java
+// https://github.com/williamfiset/Algorithms/blob/master/src/main/java/com/williamfiset/algorithms/datastructures/fenwicktree/FenwickTreeRangeUpdatePointQuery.java
+// https://www.topcoder.com/thrive/articles/Binary%20Indexed%20Trees
+// https://bitbucket.org/StableSort/play/raw/a2caef898f9f270667aac77ab51c25c9d2ed8957/src/com/stablesort/fenwick/FenwickTree.java
+
 // input should have 0 index empty
 function createSimpleFenwickTree(input) {
     const tree = [...input];
